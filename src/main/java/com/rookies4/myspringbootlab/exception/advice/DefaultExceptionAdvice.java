12f4e07d8f8490fff5b5c1200824c06cd8b88f12
sehuikim02 <lulla1613@gmail.com>
2025-08-27 23:24:19 +0900

    CorsConfig 및 BookRepositoryTest 수정

--- src/main/java/com/rookies4/myspringbootlab/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/rookies4/myspringbootlab/exception/advice/DefaultExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.rookies4.myspringbootlab.exception.advice;
 
+import com.rookies4.myspringbootlab.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
